7993ca47a2b7a687ca9be27d05d2aca5a97adeee
Urvashi Kashyap <Kashyapurvashi23@gmail.com>
2026-01-11 17:40:03 +0530

    SOLVING TWO SUM PROBLEM USING HASHMAPS

---
+++ TWO_SUM.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class TWO_SUM {
+
+    public static boolean sum(int arr[] , int target) {
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i = 0 ;i<arr.length ; i++){
+            int num = target - arr[i];
+            
+            if(map.containsKey(num)){
+                return true;
+            }
+            map.put(arr[i], i);
+        }
+        
+        return false;
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {4,7,1,9};
+        int target = 4;
+        boolean res = sum(arr,target);
+        System.out.println(res);
+    }
+}
