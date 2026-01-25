4446af0812c3b92d76da1b8f61941425407f285d
Urvashi Kashyap <Kashyapurvashi23@gmail.com>
2026-01-25 23:23:19 +0530

    FINDING THE NEXT PERMUTATION

---
+++ ARRAYS/MEDIUM/NEXT_PERMUTATION.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class NEXT_PERMUTATION {
+    public static void nextPermutation(int[] nums) {
+        int index = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+        for (int i = nums.length - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums, i, index);
+                break;
+            }
+        }
+        reverse(nums, index + 1, nums.length - 1);
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 5, 4 };
+        nextPermutation(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        System.out.println();
+    }
+}
