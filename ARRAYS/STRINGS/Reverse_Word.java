4c1e051a7ba70ddacb823edb980e9135f7272adb
Urvashi Kashyap <Kashyapurvashi23@gmail.com>
2026-02-08 18:30:44 +0530

    REVERSE THE SEQUENCE OF WORDS IN STRING

---
+++ ARRAYS/STRINGS/Reverse_Word.java
@@ -0,0 +1,16 @@
+package DSA.ARRAYS.STRINGS;
+
+public class Reverse_Word {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String[]res = str.split(" ");
+        StringBuilder ans = new StringBuilder();
+        for(int i = res.length-1; i>=0 ; i-- ){
+            ans.append(res[i]);
+            if(i!=0){
+            ans.append(" ");
+        }
+        }
+        System.out.println(ans.toString());
+    }
+}
