add4b2b584f01b517ebd513e0e015d1b74ba2504
Urvashi Kashyap <Kashyapurvashi23@gmail.com>
2026-02-16 22:35:59 +0530

    UPDATED

--- STRINGS/Largest_Odd_Number.java
+++ STRINGS/Largest_Odd_Number.java
@@ -1,12 +1,12 @@
 public class Largest_Odd_Number {
     public static void main(String[] args) {
-        String str = "271983";
+        String str = "27198";
         String ans = Sol(str);
         System.out.println(ans);
     }
      static String Sol(String str){
-        for(int i = str.length() ; i>=0 ; i--){
-            if(i%2!=0){
+        for(int i = str.length()-1 ; i>=0 ; i--){
+            if(str.charAt(i)%2!=0){
                 String str2 = str.substring(0,i+1);
                 return str2;
             }
